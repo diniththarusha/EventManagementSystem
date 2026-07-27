b5b33c1e829d213aa1b6d7c73b69cb31b1a68e79
Tharusha A.D.D <diniththarusha21@gmail.com>
2026-07-27 14:02:56 +0530

    Update IndexModel to retrieve upcoming events
    Refactor IndexModel to use EventService and fetch upcoming events asynchronously.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EventManagementSystem.Data;
+using EventManagementSystem.Models;
 
 namespace EventManagementSystem.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly EventService _eventService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(EventService eventService)
     {
-        _logger = logger;
+        _eventService = eventService;
     }
 
-    public void OnGet()
-    {
+    public List<Event> UpcomingEvents { get; set; } = new();
 
+    public async Task OnGetAsync()
+    {
+        var allEvents = await _eventService.GetAllAsync();
+        UpcomingEvents = allEvents
+            .Where(e => e.Status == "Scheduled" && e.EventDate >= DateTime.Now)
+            .OrderBy(e => e.EventDate)
+            .Take(3)
+            .ToList();
     }
 }
